5d144163e1dd896b83259ab4ac939285ee60f241
Nikolay Gorbachenko <eaglegor@gmail.com>
2022-07-31 13:58:12 +0300

    Removed wrapper

--- api/cpp/assfire/messenger/api/Payload.hpp
+++ api/cpp/assfire/messenger/api/Payload.hpp
@@ -4,38 +4,5 @@
 #include <string>
 
 namespace assfire::messenger {
-    class Payload {
-      public:
-        Payload() = default;
-        Payload(uint8_t* buf, std::size_t size) : _data(buf, size) {}
-        Payload(std::size_t size) {
-            _data.reserve(size);
-        }
-        Payload(const Payload& rhs) = default;
-        Payload(Payload&& rhs)      = default;
-
-        Payload& operator=(const Payload& rhs) = default;
-        Payload& operator=(Payload&& rhs) = default;
-
-        bool operator==(const Payload& rhs) const = default;
-
-        size_t size() const {
-            return _data.size();
-        }
-
-        const uint8_t* data() const {
-            return _data.data();
-        }
-
-        void reserve(std::size_t size) {
-            _data.reserve(size);
-        }
-
-        uint8_t* data() {
-            return _data.data();
-        }
-
-      private:
-        std::basic_string<uint8_t> _data;
-    };
+    using Payload = std::basic_string<uint8_t>;
 } // namespace assfire::messenger
