4444f6478ac5e0b7ee6e4587365649ff1739ccca
Nikolay Gorbachenko <eaglegor@gmail.com>
2022-07-31 13:27:57 +0300

    Added interrupt method to consumer

--- api/cpp/assfire/messenger/api/Consumer.hpp
+++ api/cpp/assfire/messenger/api/Consumer.hpp
@@ -7,8 +7,8 @@
 namespace assfire::messenger {
     class Consumer {
       public:
-        virtual ~Consumer()                                                    = default;
-        virtual std::optional<Message> next_message()                          = 0;
-        virtual void consume_loop(std::function<bool(const Message&)> consume) = 0;
+        virtual ~Consumer()                           = default;
+        virtual std::optional<Message> next_message() = 0;
+        virtual void interrupt()                      = 0;
     };
 } // namespace assfire::messenger
