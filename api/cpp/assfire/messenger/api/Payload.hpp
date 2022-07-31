d94fdf4581f06f63bd89e6554605df92d7006da9
Nikolay Gorbachenko <eaglegor@gmail.com>
2022-07-31 15:11:17 +0300

    Added concepts to simplify packing proto messages

--- api/cpp/assfire/messenger/api/Payload.hpp
+++ api/cpp/assfire/messenger/api/Payload.hpp
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <concepts>
 #include <cstdint>
 #include <string>
 
 namespace assfire::messenger {
     using Payload = std::basic_string<uint8_t>;
+
+    template<typename T>
+    concept ProtoMessage = requires(T m) {
+        { m.ByteSizeLong() }
+        ->std::convertible_to<std::size_t>;
+
+        { m.SerializeToArray((void *) 0, 0) }
+        ->std::convertible_to<bool>;
+    };
+
+    template<ProtoMessage T>
+    Payload pack(const T &msg) {
+        messenger::Payload payload;
+        payload.resize(msg.ByteSizeLong());
+        msg.SerializeToArray(payload.data(), payload.size());
+        return payload;
+    };
 } // namespace assfire::messenger
